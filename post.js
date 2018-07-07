a86103a6e9664ef3e8b033f0763b4f06928f6962
Hajime Hoshi <hajimehoshi@gmail.com>
2018-07-07 23:54:36 +0900

    Add a scope to hide Module

--- post.js
+++ post.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var stbvorbis = {};
-
 (Module => {
   var decodeMemory = null;
   var initialized = null;
@@ -102,3 +100,5 @@ var stbvorbis = {};
     return result;
   };
 })(Module);
+
+})(); // End of the scope.
