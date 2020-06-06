fc8eda82c036564b9deefb5616ea37f0279f37f5
Hajime Hoshi <hajimehoshi@gmail.com>
2020-06-06 15:50:33 +0900

    genjs.sh: Suspend immediately when an error occurs

--- genjs.sh
+++ genjs.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 mkdir -p build
 
 emcc -Os -o main.js -g -DSTB_VORBIS_NO_INTEGER_CONVERSION -DSTB_VORBIS_NO_STDIO \
